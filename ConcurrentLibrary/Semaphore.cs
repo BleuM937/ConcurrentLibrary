0a606bfcf365100079c173a4dd60e3af105a5386
Angus Houston <angushouston@gmail.com>
2013-03-10 20:36:00 +1100

    Add Semaphore class

---
+++ ConcurrentLibrary/Semaphore.cs
@@ -0,0 +1,35 @@
+﻿namespace ConcurrentLibrary
+{
+    using System;
+    using System.Threading;
+
+    public class Semaphore
+    {
+        private readonly object tokenLock = new object();
+        private int tokens;
+
+        public Semaphore(int tokens = 0)
+        {
+            this.tokens = tokens;
+        }
+
+        public void Acquire()
+        {
+            lock (tokenLock)
+            {
+                while (tokens == 0)
+                    Monitor.Wait(tokenLock);
+                --tokens;
+            }
+        }
+
+        public void Release()
+        {
+            lock (tokenLock)
+            {
+                ++tokens;
+                Monitor.PulseAll(tokenLock);
+            }
+        }
+    }
+}
