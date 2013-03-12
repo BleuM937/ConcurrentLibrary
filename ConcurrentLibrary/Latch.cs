94c7f96f36539de88f52b12f9fb2b63f4e5ebaa5
Angus Houston <angushouston@gmail.com>
2013-03-12 20:30:23 +1100

    Implemented Latch class

---
+++ ConcurrentLibrary/Latch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConcurrentLibrary
+{
+    public class Latch
+    {
+        private Semaphore ready = new Semaphore(0);
+
+        public void Acquire()
+        {
+            ready.Acquire();
+            ready.Release();
+        }
+
+        public void Release()
+        {
+            ready.Release();
+        }
+    }
+}
