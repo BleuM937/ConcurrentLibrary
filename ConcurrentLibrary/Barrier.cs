e2f3a79d0f020671dfc462e37c7e98b711816067
Angus Houston <angushouston@gmail.com>
2013-03-19 19:51:11 +1100

    Implemented and tested Barrier

---
+++ ConcurrentLibrary/Barrier.cs
@@ -0,0 +1,34 @@
+﻿namespace ConcurrentLibrary
+{
+    using System;
+    
+    public class Barrier
+    {
+        private readonly object barrierLock = new object();
+        Semaphore turnstile = new Semaphore(1);
+        Semaphore ready = new Semaphore(0);
+        int numWaiting = 0;
+        int maxWaiting;
+
+        public Barrier(int waitCount) { maxWaiting = waitCount; }
+
+        public void Arrive()
+        {
+            turnstile.Acquire();
+            lock (barrierLock)
+            {
+                ++numWaiting;
+                if (numWaiting == maxWaiting)
+                    ready.Release(maxWaiting);
+                turnstile.Release();
+            }
+            ready.Acquire();
+            lock (barrierLock)
+            {
+                --numWaiting;
+                if (numWaiting == 0)
+                    turnstile.Release();
+            }
+        }
+    }
+}
