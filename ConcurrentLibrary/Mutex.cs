60bd727b2fb0c76710945270f1f358314e1e9404
Angus Houston <angushouston@gmail.com>
2013-03-26 08:59:43 +1100

    Implemented Mutex

---
+++ ConcurrentLibrary/Mutex.cs
@@ -0,0 +1,24 @@
+﻿namespace ConcurrentLibrary
+{
+    using System;
+
+    public class Mutex
+    {
+        private readonly Semaphore enter = new Semaphore(1);
+        private bool entered;
+
+        public void Acquire()
+        {
+            enter.Acquire();
+            entered = true;
+        }
+
+        public void Release()
+        {
+            if (!entered) throw new 
+                InvalidOperationException("Cannot Release a Mutex before it has been acquired.");
+            entered = false;
+            enter.Release();
+        }
+    }
+}
