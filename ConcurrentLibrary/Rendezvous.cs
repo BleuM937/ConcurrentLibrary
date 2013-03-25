80b0c98025383f2d6be1159dd9b00722afb841ea
Angus Houston <angushouston@gmail.com>
2013-03-25 19:05:55 +1100

    Corrected Rendezvous implementation
    Added the ability to exchange an item between two threads.

---
+++ ConcurrentLibrary/Rendezvous.cs
@@ -0,0 +1,41 @@
+﻿namespace ConcurrentLibrary
+{
+    public class Rendezvous<T>
+    {
+        private readonly object exchangeLock = new object();
+        Semaphore ready = new Semaphore();
+        Semaphore turnstile = new Semaphore(1);
+        T data;
+        bool filled;
+
+        public T Exchange(T item)
+        {
+            T result;
+            turnstile.Acquire();
+            lock (exchangeLock)
+            {
+                if (filled)
+                {
+                    result = data;
+                    data = item;
+                    ready.Release();
+                    return result;
+                }
+                else
+                {
+                    data = item;
+                    filled = true;
+                    turnstile.Release();
+                }
+            }
+            ready.Acquire();
+            lock (exchangeLock)
+            {
+                filled = false;
+                result = data;
+            }
+            turnstile.Release();
+            return result;
+        }
+    }
+}
