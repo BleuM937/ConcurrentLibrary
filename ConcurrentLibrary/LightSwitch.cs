69985bfea5d8c7a3055853f8d6698de2a9170670
Angus Houston <angushouston@gmail.com>
2013-03-26 19:04:11 +1100

    Added LightSwitch test

---
+++ ConcurrentLibrary/LightSwitch.cs
@@ -0,0 +1,23 @@
+﻿namespace ConcurrentLibrary
+{
+    public class LightSwitch
+    {
+        private readonly Semaphore target;
+        private readonly object countLock = new object();
+        private int count;
+
+        public LightSwitch(Semaphore target) { this.target = target; }
+
+        public void Acquire()
+        {
+            lock (countLock) 
+                if (count++ == 0) target.Acquire();
+        }
+
+        public void Release()
+        {
+            lock (countLock)
+                if (--count == 0) target.Release();
+        }
+    }
+}
