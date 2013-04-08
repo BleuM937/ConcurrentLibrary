23bcacf3b3ce4b78eeb131ea1e593de82bf558bb
Angus Houston <angushouston@gmail.com>
2013-04-08 11:20:46 +1000

    Implemented ReaderWriterLock

---
+++ ConcurrentLibrary/ReaderWriterLock.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConcurrentLibrary
+{
+    using System;
+
+    public class ReaderWriterLock
+    {
+        private readonly Semaphore
+            readTurnstile = new Semaphore(1),
+            writeTurnstile = new Semaphore(1),
+            writePerm = new Semaphore(1);
+        private readonly LightSwitch readPerm;
+
+        public ReaderWriterLock()
+        {
+            readPerm = new LightSwitch(writePerm);
+        }
+
+        public void AcquireWrite()
+        {
+            writeTurnstile.Acquire();
+            readTurnstile.Acquire();
+            writePerm.Acquire();
+        }
+
+        public void ReleaseWrite()
+        {
+            writePerm.Release();
+            readTurnstile.Release();
+            writeTurnstile.Release();
+        }
+
+        public void AcquireRead()
+        {
+            readTurnstile.Acquire();
+            readTurnstile.Release();
+            readPerm.Acquire();
+
+        }
+        public void ReleaseRead()
+        {
+            readPerm.Release();
+        }
+    }
+}
