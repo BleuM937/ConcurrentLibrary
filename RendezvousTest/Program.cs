37302df1d0fd2b67f91911d5f12e80c70f7c18ee
Angus Houston <angushouston@gmail.com>
2013-05-07 14:30:07 +1000

    Fixed Rendezvous test to clearer demonstration
    Added coloured console output, matched with thread inputs and outputs.

--- RendezvousTest/Program.cs
+++ RendezvousTest/Program.cs
@@ -10,21 +10,37 @@
         {
             Rendezvous<string> rendezvous = new Rendezvous<string>();
 
+            var inputColors = new[] 
+            {
+                ConsoleColor.DarkRed,
+                ConsoleColor.DarkGreen,
+                ConsoleColor.DarkBlue,
+                ConsoleColor.DarkYellow            
+            };
+            var outputColors = new[]
+            {
+                ConsoleColor.Red,
+                ConsoleColor.Green,
+                ConsoleColor.Blue,
+                ConsoleColor.Yellow
+            };
+
             for (int i = 0; i < 4; i++)
             {
+                Thread.Sleep(1000);
                 new Thread((param) =>
+                {
+                    int n = (int)param;
+                    Random random = new Random();
+                    for (int j = 0; true; j++)
                     {
-                        int n = (int)param;
-                        Random random = new Random();
-                        for (int j = 0; true; j++)
-                        {
-                            Thread.Sleep(random.Next(2500, 4000));
-                            string input = string.Concat(n, ":", j);
-                            Console.WriteLine("Thread {0} --> {1}", n, input);
-                            string output = rendezvous.Exchange(input);
-                            Console.WriteLine("Thread {0} <-- {1}", n, output);
-                        }
-                    }).Start(i);
+                        Thread.Sleep(random.Next(2500, 4000));
+                        string input = string.Concat(n, ":", j);
+                        WriteLineWithColor(inputColors[n], "Thread {0} --> {1}", n, input);
+                        string output = rendezvous.Exchange(input);
+                        WriteLineWithColor(outputColors[n], "Thread {0} <-- {1}", n, output);
+                    }
+                }).Start(i);
             }
         }
 
