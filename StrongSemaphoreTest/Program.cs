8d4fbc957fc3ed18ce9e753578961684efbcb3c0
Angus Houston <angushouston@gmail.com>
2013-05-07 13:39:03 +1000

    Added StrongSemaphore test

---
+++ StrongSemaphoreTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace StrongSemaphoreTest
+{
+    using ConcurrentLibrary;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StrongSemaphore semaphore = new StrongSemaphore();
+
+            new Thread(() =>
+            {
+                Random r = new Random();
+
+                for (int i = 0; ; i++)
+                {
+                    new Thread(param =>
+                    {
+                        var sem = param as StrongSemaphore;
+                        Console.WriteLine("Acquiring: " + Thread.CurrentThread.Name);
+                        sem.Acquire();
+                        Console.WriteLine("Acquired: " + Thread.CurrentThread.Name);
+                    }) { Name = "Thread " + i }
+                    .Start(semaphore);
+                    Thread.Sleep(r.Next(500, 1000));
+                }
+            }).Start();
+
+            while (true)
+            {
+                Console.ReadLine();
+                Console.WriteLine(new String('-', 79));
+                semaphore.Release();
+            }
+        }
+    }
+}
